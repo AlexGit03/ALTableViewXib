e6bbeccee89e82a80e41f8ea15896ba0b1e8e3d9
Alessandro <alessandro.chillura@progettopa.it>
2020-10-02 14:23:09 +0200

    bundle path swift package manager

--- Sources/ALTableViewXib/UIView+Extension.swift
+++ Sources/ALTableViewXib/UIView+Extension.swift
@@ -5,7 +5,11 @@ import UIKit
 extension UIView {
     
     public func loadNib() -> UIView {
-        let bundle = Bundle(for: type(of: self) as AnyClass)
+        guard let xibPath = Bundle.module.path(forResource: self.className, ofType: "xib") else  {
+            print("errore nel prendere il path della classe \(self.className)")
+            return UIView()
+        }
+        let bundle = Bundle.init(path: xibPath)
         let nib = UINib(nibName: self.className, bundle: bundle)
         let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
         return view
